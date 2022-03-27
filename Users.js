cac18aaeff7b4024cff46c2865ca25759048b607
João Gutierre <gutierrejoao@gmail.com>
2022-03-27 03:19:53 -0300

    feat: Criando server e testando requisição GET

---
+++ Users.js
@@ -0,0 +1,17 @@
+const users = [
+    {
+        id: 1,
+        name: 'João Gutierre',
+        email: 'gutierrejoao@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Letícia Almeida Lima',
+        email: 'leticia.almeida@outlook.com.br'
+    },
+    {
+        id: 3,
+        name: 'Emily Humger Bastos',
+        email: 'emily.bastos@outlook.com.br'
+    }
+]
