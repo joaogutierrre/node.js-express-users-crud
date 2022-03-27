1b51328d4bc6d977be533d4ccd5b43e481f7461a
João Gutierre <gutierrejoao@gmail.com>
2022-03-27 03:37:32 -0300

    feat: Criando rota GET p/ lista de usuários

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,3 +2,8 @@ const express = require('express')
 const router = express.Router()
 const uuid = require('uuid')
 let users = require('../../Users')
+
+//get all users
+router.get('/', (req,res) =>{
+    res.json(users);
+});
