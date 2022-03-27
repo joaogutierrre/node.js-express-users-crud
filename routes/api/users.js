6136e38422c56d89509a1d12d3a5bbb942b57bfa
João Gutierre <gutierrejoao@gmail.com>
2022-03-27 04:11:46 -0300

    feat: Filtrando GET por ID de usuário

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,4 +8,16 @@ router.get('/', (req, res) => {
   res.json(users)
 })
 
+//get user by id
+
+router.get('/:id', (req, res) => {
+  const found = users.some(user => user.id === parseInt(req.params.id))
+
+  if (found) {
+    res.json(users.filter(user => user.id === parseInt(req.params.id)))
+  } else {
+    res.sendStatus(400)
+  }
+})
+
 module.exports = router
