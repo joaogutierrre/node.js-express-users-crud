3b55780864df6d7dfec73d2ad9e4294c97889514
João Gutierre <gutierrejoao@gmail.com>
2022-03-27 03:49:38 -0300

    feat: Executando GET na lista estática de usuários

--- Users.js
+++ Users.js
@@ -1,24 +1,24 @@
 const users = [
-    {
-        id: 1,
-        name: 'João Gutierre',
-        email: 'gutierrejoao@gmail.com'
-    },
-    {
-        id: 2,
-        name: 'Letícia Almeida Lima',
-        email: 'leticia.almeida@outlook.com.br'
-    },
-    {
-        id: 3,
-        name: 'Emily Humger Bastos',
-        email: 'emily.bastos@outlook.com.br'
-    },
-    {
-        id: 4,
-        name: 'Ygor Bandeira',
-        email: 'ygor.bandeira@outlook.com.br'
-    }
+  {
+    id: 1,
+    name: 'João Gutierre',
+    email: 'gutierrejoao@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Letícia Almeida Lima',
+    email: 'leticia.almeida@outlook.com.br'
+  },
+  {
+    id: 3,
+    name: 'Emily Humger Bastos',
+    email: 'emily.bastos@outlook.com.br'
+  },
+  {
+    id: 4,
+    name: 'Ygor Bandeira',
+    email: 'ygor.bandeira@outlook.com.br'
+  }
 ]
 
-module.exports = users;
+module.exports = users
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const router = express.Router()
 const uuid = require('uuid')
+const router = express.Router()
 let users = require('../../Users')
 
 //get all users
-router.get('/', (req,res) =>{
-    res.json(users);
-});
+router.get('/', (req, res) => {
+  res.json(users)
+})
+
+module.exports = router
